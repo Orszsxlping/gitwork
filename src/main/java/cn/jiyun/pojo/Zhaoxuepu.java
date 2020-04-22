23892cdc92fe0c515aa9f0cbbba7d83e1e18d8c3
zhao.xuepu <zhao.xuepu@163.com>
2020-04-22 10:16:40 +0800

    项目提交

---
+++ src/main/java/cn/jiyun/pojo/Zhaoxuepu.java
@@ -0,0 +1,21 @@
+package cn.jiyun.pojo;
+
+public class Zhaoxuepu {
+
+    /**
+     * 1:每人提交一个文件
+     *   类名命名以自己名字全拼 例如：Liushihui.java
+     *   类放到pojo包下
+     *   完成git仓库提交
+     *
+     * */
+    public String userName;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+}
